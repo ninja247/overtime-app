355ea4873c79f76067583be612c077df6b276402
ninja247 <andrewericoleman@gmail.com>
2017-11-13 20:11:38 -0600

    ssh test 2

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
 	sequence :email do |n|
     "test#{n}@example.com"
   end
-  
+
   factory :user do
     first_name 'Jon'
     last_name 'Snow'
@@ -11,6 +11,7 @@ FactoryGirl.define do
     password_confirmation "asdfasdf"
   end
 
+  
   factory :admin_user, class: "AdminUser" do
     first_name 'Admin'
     last_name 'User'
