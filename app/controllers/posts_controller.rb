95580c197e309b192887842b6f64f6edcf3eca50
ninja247 <andrewericoleman@gmail.com>
2017-11-15 16:13:18 -0600

    Implemented creation process for posts and refactored

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-	before_action :set_post, only: [:show]
+	before_action :set_post, only: [:show, :edit]
 	
 	def index
 		@posts = Post.all
@@ -20,6 +20,18 @@ class PostsController < ApplicationController
 		end
 	end
 
+	
+	def edit
+	end
+	
+	def update
+		@posts.update(post_params)
+			redirect_to @post, notice: 'Your post was created successfully'
+		else
+			render :edit
+		end
+	end
+	
 	def show
 	end
 
